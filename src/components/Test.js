ec3b7d697c7b941086e954d5f9e740e8b33b703e
Ramon Zubiate <ramon.zubiate@tylertech.com>
2023-01-13 18:07:45 -0600

    Delete src directory

--- src/components/Test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-//renders "Hello, World!" (meant for testing purposes ONLY)
-//props expected: N/A
-export default function Test(){
-    return(
-        <h1>Hello, World!</h1>
-    );
-}
